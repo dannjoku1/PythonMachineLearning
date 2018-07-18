e0e4d877a7d4acb63b0c88d2f122a1c7c2ac8144
Dan Njoku <dannjoku1@gmail.com>
2018-07-18 07:10:50 -0400

    Price prediction improvements

--- regression-basics.py
+++ regression-basics.py
@@ -1,5 +1,6 @@
 import pandas
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
@@ -8,4 +9,12 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) /df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+# column shifting for adjusted price changes
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+print(df.tail())
