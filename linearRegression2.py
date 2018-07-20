d4f518641f97ee4bdc1d631fa7d73edbbdc9e32a
Dan Njoku <dannjoku1@gmail.com>
2018-07-20 12:38:24 -0400

    finished calculating slope

--- linearRegression2.py
+++ linearRegression2.py
@@ -1,19 +1,28 @@
 from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,5,6,3,7], dtype=np.float64)
+xs = np.array([0,1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,5,6,3,7,9], dtype=np.float64)
 
 #y = mx + b
-def best_fit_slope(xs, ys):
-    m = ( ((mean(xs) * mean(ys)) - mean(xs*ys)) /
-          ((mean(xs)*mean(xs)) - mean(xs*xs)) )
-    return m
-m = best_fit_slope(xs, ys)
+def best_fit_slope_and_intercept(xs, ys):
+    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) /
+        ((mean(xs)*mean(xs)) - mean(xs*xs)))
+    b = mean(ys) - m*mean(xs)
+    return m,b
 
-print(m)
+m, b = best_fit_slope_and_intercept(xs, ys)
 
-#plt.scatter(xs, ys)
-#plt.show()
+
+regression_line = [(m*x)+b for x in xs]
+
+predict_x = 12
+predict_y = (m*predict_x)+b
+
+plt.scatter(xs, ys)
+plt.scatter(predict_x, predict_y)
+plt.plot(xs, regression_line)
+plt.show()
 
